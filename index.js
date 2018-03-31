c2ef8aeae8eb10b9ec501e149faa607da0dc3197
Adjectival <alexd.jacks@gmail.com>
2018-03-31 16:52:10 -0700

    :tada: first commit, adds app shell and readme

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// base libraries
+import React from 'react';
+import { View, Text, AppRegistry, StyleSheet } from 'react-native';
 
+
+// create component
+const App = () => (
+	<View style={styles.container}>
+		<Text style={styles.text}>HUZZAH</Text>
+		<Text style={styles.text}>HUZZAH</Text>
+		<Text style={styles.text}>HUZZAH</Text>
+		<Text style={styles.text}>HUZZAH</Text>
+		<Text style={styles.text}>HUZZAH</Text>
+		<Text style={styles.text}>HUZZAH</Text>
+	</View>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'papayawhip',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'firebrick'
+    }
+});
+
+// render to devices
 AppRegistry.registerComponent('albums', () => App);
---
+++ readme.md
@@ -0,0 +1,17 @@
+# React-Native Catalog :iphone::cd: 🎮
+
+## Adjectival
+
+## Function
+
+Display catalog of cover art and game titles for unofficial video games.
+
+Build basic react-native app composition skills.
+
+## Install
+
+Not available. Please google "react-native install" for hours of rewarding adventures.
+
+## Development Goals
+
+Refactor into reusable boilerplate for RN development.
