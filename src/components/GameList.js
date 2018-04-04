d177285bd68032e5aaf345b3306e8f6a898b15e2
Adjectival <alexd.jacks@gmail.com>
2018-04-04 15:09:31 -0700

    :triangular_flag_on_post: installs axios lib and fetches data from mock API

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,19 +1,25 @@
 // import libraries
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import axios from 'axios';
 import Game from './Game';
 
 // make component
-const GameList = () => {
-	return (
-	<View style={styles.list}>
-		<Game/>
-		<Game/>
-		<Game/>
-		<Game/>
-	</View>
-	);
-};
+class GameList extends Component {
+	componentWillMount() {
+		axios.get('https://raw.githubusercontent.com/Adjectival/RN-catalog/master/src/mock_api.json')
+			.then(response => console.log(response))
+	}
+
+	render() {
+		return (
+		<View style={styles.list}>
+			<Text>Game List</Text>
+			<Game/>
+		</View>
+		);
+	}
+}
 
 const styles = StyleSheet.create({
 	list: {
